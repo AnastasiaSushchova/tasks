f8648d9ebba90921ad3770ee01b03859686fb6a5
AnastasiaSushchova <96011646+AnastasiaSushchova@users.noreply.github.com>
2022-10-12 22:12:09 +0300

    add arrays task

---
+++ freecodecamp.js
@@ -0,0 +1,12 @@
+// Manipulate Arrays With pop()
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+
+// Manipulate Arrays With shift()
+const myShiftArray = [["John", 23], ["dog", 3]];
+const removedShiftFromMyArray = myArray.shift();
+
+// Manipulate Arrays With unshift()
+const myUnshiftArray = [["John", 23], ["dog", 3]];
+myUnshiftArray.shift();
+myUnshiftArray.unshift(["Paul", 35]);
