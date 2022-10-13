465c6d456ab2d18285229db40071ce3ffd0ec923
AnastasiaSushchova <96011646+AnastasiaSushchova@users.noreply.github.com>
2022-10-13 23:47:37 +0300

    add new task

--- freecodecamp.js
+++ freecodecamp.js
@@ -10,3 +10,10 @@ const removedShiftFromMyArray = myArray.shift();
 const myUnshiftArray = [["John", 23], ["dog", 3]];
 myUnshiftArray.shift();
 myUnshiftArray.unshift(["Paul", 35]);
+
+// Assignment with a Returned Value
+let processed = 0;
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
