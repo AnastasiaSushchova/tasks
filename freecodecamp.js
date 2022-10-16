98e85112dc5024e2ede587280c6c0dbc2b060b9e
AnastasiaSushchova <96011646+AnastasiaSushchova@users.noreply.github.com>
2022-10-16 22:32:41 +0300

    new switch task

--- freecodecamp.js
+++ freecodecamp.js
@@ -107,5 +107,68 @@ break;
   return answer;
 }
 
+// Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+switch (val){
+  case "a":
+  answer = "apple";
+  break;
+  case "b":
+  answer = "bird";
+  break;
+  case "c":
+  answer = "cat";
+  break;
+  default:
+  answer = "stuff";
+  break;
+}
+  return answer;
+}
 
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+  answer = "Low";
+  break;
+  case 4:
+  case 5:
+  case 6:
+  answer = "Mid";
+  break;
+  case 7:
+  case 8:
+  case 9:
+  answer = "High";
+  break;
+}
+  return answer;
+}
 
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+switch (val) {
+  case "bob":
+  answer = "Marley";
+  break;
+  case 42:
+  answer = "The Answer";
+  break;
+  case 1:
+  answer = "There is no #1";
+  break;
+  case 99:
+  answer = "Missed me by this much!";
+  break;
+  case 7:
+  answer = "Ate Nine";
+  break;
+}
+  return answer;
+}
