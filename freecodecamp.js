988c620c7c5dbe6d32116e7f06cde2ff296f7811
AnastasiaSushchova <96011646+AnastasiaSushchova@users.noreply.github.com>
2022-10-16 23:11:21 +0300

    counting card task

--- freecodecamp.js
+++ freecodecamp.js
@@ -172,3 +172,35 @@ switch (val) {
 }
   return answer;
 }
+
+//Counting Cards
+let count = 0;
+
+function cc(card) {
+switch (card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+   count++;
+  break;
+  case 7:
+  case 8:
+  case 9:
+  count;
+  break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+  count--;
+  break;
+}
+if (count > 0){
+  return count + " Bet";
+} else{
+  return count + " Hold";
+}
+}
